9b9791cb92c9b015ec85723b5d70c36835334591
Tapan Goel <tapangoel1994@gmail.com>
2024-06-26 12:50:35 -0400

    Updated colorpalette to use latex interpreter for all text and udpated marker types for lytic, lysogenic and temperate virus

--- Code/lib/colorpalette.m
+++ Code/lib/colorpalette.m
@@ -7,12 +7,28 @@ linecolors.I = '#a74ac7';
 linecolors.L = '#00cbff';
 linecolors.V = '#ff0000';
 
-marker.lytic = 'o';
+marker.lytic = 'd';
 marker.temperate = 's';
-marker.lysogenic = '^';
+marker.lysogenic = 'o';
 
 linestyle.lytic = '-';
 linestyle.temperate = '-.';
 linestyle.lysogenic = ':';
+
+% Set default interpreters to 'latex' for consistent font styling
+% This script changes all interpreters from tex to latex. 
+
+list_factory = fieldnames(get(groot,'factory'));
+index_interpreter = find(contains(list_factory,'Interpreter'));
+
+for i = 1:length(index_interpreter)
+    default_name = strrep(list_factory{index_interpreter(i)},'factory','default');
+    set(groot, default_name,'latex');
+end
+% Optionally, set a consistent font style, e.g., 'Times New Roman'
+set(groot, 'defaultAxesFontName', 'Times New Roman');
+set(groot, 'defaultTextFontName', 'Times New Roman');
+
+
 
 save('colorpalette.mat');
