f6fa96af0ba5be70f9c40ef324f292e5a5ac91a4
Tapan Goel <tapangoel1994@gmail.com>
2024-07-21 09:35:15 -0400

    Added scripts to generate SI figures

---
+++ Code/Figure4SI.m
@@ -0,0 +1,117 @@
+%% Script generates steady state viral genome density plots as function of Q
+%% for the value of gamma over which the invasion dynamics have been calculated.
+%% this is to check that the excluded regions in the invasion analysis make sense. 
+
+
+%% Author: Tapan Goel
+%% Date: 7/18/2024
+
+clear all;
+close all;
+
+addpath("lib\");
+addpath("utils\");
+colorpalette;
+fixedparameters;
+
+
+%% Generate genome density plots corresponding to figure 4
+
+CyclePeriod = 24;
+Gamma = logspace(-3,0,51);
+Q = linspace(0,1,51);
+NumNodes = 12;
+
+%%Add missing simulation parameters
+params.T = CyclePeriod; % hours
+params.t_vals = transpose(0:params.dt:params.T); % time
+
+p_LV = [0 .2;.2 0];
+S0 = 1e7;
+Va_0 = 1e4;
+Vb_0 = 10*criticaldensitythreshold;
+
+for index = 1:2
+    if isfile(sprintf("..\\Data\\SteadyState_CyclePeriod=%.1f,S0=%1.e,V0=%1.e,p_L=%.1f,p_V=%.1f.mat",CyclePeriod,S0,Va_0,p_LV(index,1),p_LV(index,2)))
+        load(sprintf("..\\Data\\SteadyState_CyclePeriod=%.1f,S0=%1.e,V0=%1.e,p_L=%.1f,p_V=%.1f.mat",CyclePeriod,S0,Va_0,p_LV(index,1),p_LV(index,2)));
+        SteadyState{index} = SteadyStateDensity;
+        CyclesSteadyState{index} = SSCycles;
+    else
+        [SteadyState{index}, CyclesSteadyState{index}] = PopulationSteadyStateFunction(CyclePeriod,p_LV(index,1),p_LV(index,2),Gamma,Q,NumNodes,1,params);
+    end
+end
+
+% Plot the steady state viral genome densities of resident along the fixed
+% gamma line
+
+h = figure('Renderer','painters','Position',[760 200 1225 951/2]);
+t = tiledlayout(1,2,'Padding','loose','TileSpacing','loose');
+
+inset_position = [0.1600    0.3000    0.1994    0.4000; .62 .33 .1994 .4];
+colorlabel_position = [2.9109 4.4107e+06 0; 2.9109 3e2 0];
+for index = 1:2
+    
+    SteadyState_temp = SteadyState{index};
+    [~,i] = min(abs(Gamma - 0.0832));
+    
+    nexttile(t,index);
+    semilogy(Q,sum(SteadyState_temp(:,i,3:2:10),3),'LineWidth',2,'Color','k','Marker','o','MarkerFaceColor','k','MarkerSize',4);
+    hold on;
+    yline(100*criticaldensitythreshold,'Color',[.5 .5 .5],'LineWidth',1.5,'LineStyle','--');
+    ylim([1e-2 10*max(sum(SteadyState_temp(:,i,3:2:10),3))]);
+
+    set(gca,'YMinorTick','off','Box','off','XTick',0:.25:1,'YTick',logspace(-2,10,7),'XColor','r','LineWidth',2);
+    set(gca,'FontSize',16,'FontWeight','bold','FontName','Times');
+    hold off;
+
+    %% Add inset figure
+    ax_inset{index} = axes('Position',inset_position(index,:));
+    imagesc(Gamma,Q,sum(SteadyState_temp(:,:,3:2:10),3));    
+    hold on;
+    %plot(Gamma(i),Q(j),'*k','MarkerSize',5,'LineWidth',2);
+    contour(Gamma,Q,sum(SteadyState_temp(:,:,3:2:10),3),'k');
+    xline(.0832,'LineWidth',1.5,'Color','r');
+    xticks([1e-3 1e-2 1e-1 1e0]);
+    xticklabels({'10$^{-3}$','10$^{-2}$','10$^{-1}$','1'});
+    yticks(linspace(0,1,5));
+    c = colorbar;
+    c.Label.String = "Viral genome density (mL$^{-1}$)";
+    c.Label.Interpreter = 'latex';
+    c.Label.Rotation = -90;
+    c.Label.Position = colorlabel_position(index,:);
+    
+    set(ax_inset{index},'XScale','log','YScale','linear','ColorScale','log','YDir','normal'); 
+    xlabel('$\gamma$: Inducation rate (hr$^{-1}$)');
+    ylabel( '$p$: Integration Probability');
+    pbaspect(ax_inset{index},[1 1 1]);
+    hold off;
+
+end
+
+xlabel(t,'$p$: Integration probability','FontSize',18,'Interpreter','latex');
+ylabel(t,{'Viral genome density (mL$^{-1}$)'},'FontSize',18,'Interpreter','latex');
+nexttile(t,1);
+title('Filtrate: virions only','FontSize',20);
+nexttile(t,2);
+title('Filtrate: lysogens only','FontSize',20);
+
+%%Add panel labels
+%% Add plot labels
+nexttile(t,1);
+text(-.21,3e10,'(A)','FontSize',20,'FontWeight','bold');
+text(1.15,3e10,'(B)','FontSize',20,'FontWeight','bold');
+
+
+%% Save Figure
+filename = dir('..\Figures\SteadyStatePlusInvasion_SI*');
+
+if isempty(filename)
+    filename = '..\Figures\SteadyStatePlusInvasion_SI_v1.eps';
+else
+    filename = ['..\Figures\' filename(end).name];
+    version = extractBetween(filename,"_v",".");
+    version = version{1};
+    version = str2num(version);
+    filename = [extractBefore(filename,['v' num2str(version)]) 'v' num2str(version+1) '.eps'];
+end
+exportgraphics(h,filename,"BackgroundColor",'none','ContentType','vector');
---
+++ Code/Figure5SI.m
@@ -0,0 +1,115 @@
+%% Script generates steady state viral genome density plots as function of Q
+%% for the value of gamma over which the invasion dynamics have been calculated.
+%% this is to check that the excluded regions in the invasion analysis make sense. 
+
+
+%% Author: Tapan Goel
+%% Date: 7/18/2024
+
+
+close all; 
+clear all;
+
+addpath('utils\');
+addpath('lib\');
+colorpalette;
+fixedparameters;
+
+
+CyclePeriodList = [12,16,24];
+Gamma = logspace(-3,0,51);
+Q = linspace(0,1,51);
+NumNodes = 12;
+p_LV = [.2 0;.2 0;.2 0];
+S0 = 1e7;
+V01 = 1e4;
+
+
+for index = 1:3
+    if isfile(sprintf("..\\Data\\SteadyState_CyclePeriod=%.1f,S0=%1.e,V0=%1.e,p_L=%.1f,p_V=%.1f.mat",CyclePeriodList(index),S0,V01,p_LV(index,1),p_LV(index,2)))
+        load(sprintf("..\\Data\\SteadyState_CyclePeriod=%.1f,S0=%1.e,V0=%1.e,p_L=%.1f,p_V=%.1f.mat",CyclePeriodList(index),S0,V01,p_LV(index,1),p_LV(index,2)));
+        SteadyState{index} = SteadyStateDensity;
+        CyclesSteadyState{index} = SSCycles;
+    else
+        [SteadyState{index}, CyclesSteadyState{index}] = PopulationSteadyStateFunction(CyclePeriodList(index),p_LV(index,1),p_LV(index,2),Gamma,Q,NumNodes,1,params);
+    end
+end
+
+% Plot the steady state viral genome densities of resident along the fixed
+% gamma line
+
+h = figure('Renderer','painters','Position',[760 200 1225*3/2 951/2]);
+t = tiledlayout(1,3,'Padding','loose','TileSpacing','loose');
+
+inset_position = [0.1150    0.335    0.18    0.4000; .425 .335 .18 .4; .71 .335 .18 .4];
+colorlabel_position = [2.9109 1e4 0; 2.9109 4e3 0; 2.9109 4e2 0];
+PlotTitles = {"Cycle period = 12hr","Cycle period = 16hr","Cycle period = 24hr"};
+for index = 1:3
+    
+    SteadyState_temp = SteadyState{index};
+    SteadyState_temp = squeeze(sum(SteadyState_temp(:,:,3:10),3));
+    
+    M = max(SteadyState_temp,[],"all","linear");
+    [r,c] = find(SteadyState_temp == M);
+    j = max(r);
+    i = max(c);
+    
+    nexttile(t,index);
+    semilogy(Q,SteadyState_temp(:,i),'LineWidth',2,'Color','k','Marker','o','MarkerFaceColor','k','MarkerSize',4);
+    hold on;
+    yline(100*criticaldensitythreshold,'Color',[.5 .5 .5],'LineWidth',1.5,'LineStyle','--');
+    ylim([1e-2 10*max(SteadyState_temp(:,i))]);
+
+    set(gca,'YMinorTick','off','Box','off','XTick',0:.25:1,'YTick',logspace(-2,10,7),'XColor','r','LineWidth',2);
+    set(gca,'FontSize',16,'FontWeight','bold','FontName','Times');
+    title(PlotTitles{index},'FontSize',20);
+    hold off;
+
+    %% Add inset figure
+    ax_inset{index} = axes('Position',inset_position(index,:));
+    imagesc(Gamma,Q,SteadyState_temp);    
+    hold on;
+    %plot(Gamma(i),Q(j),'*k','MarkerSize',5,'LineWidth',2);
+    contour(Gamma,Q,SteadyState_temp,'k');
+    xline(Gamma(i),'LineWidth',1.5,'Color','r');
+    xticks([1e-3 1e-2 1e-1 1e0]);
+    xticklabels({'10$^{-3}$','10$^{-2}$','10$^{-1}$','1'});
+    yticks(linspace(0,1,5));
+    c = colorbar;
+    c.Label.String = "Viral genome density (mL$^{-1}$)";
+    c.Label.Interpreter = 'latex';
+    c.Label.Rotation = -90;
+    c.Label.Position = colorlabel_position(index,:);
+    
+    set(ax_inset{index},'XScale','log','YScale','linear','ColorScale','log','YDir','normal'); 
+    xlabel('$\gamma$: Inducation rate (hr$^{-1}$)');
+    ylabel( '$p$: Integration Probability');
+    pbaspect(ax_inset{index},[1 1 1]);
+    hold off;
+
+end
+
+xlabel(t,'$p$: Integration probability','FontSize',18,'Interpreter','latex');
+ylabel(t,{'Viral genome density (mL$^{-1}$)'},'FontSize',18,'Interpreter','latex');
+
+%% Add plot labels
+nexttile(t,1);
+text(-.21,1e8,'(A)','FontSize',20,'FontWeight','bold');
+text(1.13,1e8,'(B)','FontSize',20,'FontWeight','bold');
+text(2.42,1e8,'(C)','FontSize',20,'FontWeight','bold');
+
+%% Save Figure
+filename = dir('..\\Figures\\ChangingCyclePeriod_SI*');
+
+if isempty(filename)
+    filename = '..\\Figures\\ChangingCyclePeriod_SI_v1.eps';
+else
+    filename = [filename(end).folder '\' filename(end).name];
+    version = extractBetween(filename,"_v",".");
+    version = version{1};
+    version = str2num(version);
+    filename = [extractBefore(filename,['v' num2str(version)]) 'v' num2str(version+1) '.eps'];
+end
+exportgraphics(h,filename,"BackgroundColor",'none','ContentType','vector');
+
+
