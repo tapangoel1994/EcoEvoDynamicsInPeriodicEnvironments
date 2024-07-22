2370e2dab7b55cf7b899b362c143649717928085
Tapan Goel <tapangoel1994@gmail.com>
2024-07-22 11:14:32 -0400

    Added comments to colorpalette.m and fixedparameters.m for readability

--- Code/lib/colorpalette.m
+++ Code/lib/colorpalette.m
@@ -1,5 +1,8 @@
-%% This script generates the marker and color palatte used for timeseries plots across this project
+%% This script generates the color scheme used for creating graphics accross this project.
+%% Before running any code to generate graphs etc, run this script. 
 
+
+%% Colors for R, S, E, I, L and V in line plots
 linecolors.R = .5*[1 1 1];
 linecolors.S = '#0000ff';
 linecolors.E = '#ffa600';
@@ -7,6 +10,7 @@ linecolors.I = '#c568e5';%'#e386ff';%'#a74ac7';
 linecolors.L = '#00cbff';
 linecolors.V = '#ff0000';
 
+%% Markers  and line styles for lytic, temperate and lysogenic strategies
 marker.lytic = 'd';
 marker.temperate = 's';
 marker.lysogenic = 'o';
@@ -15,8 +19,9 @@ linestyle.lytic = '-';
 linestyle.temperate = '-.';
 linestyle.lysogenic = ':';
 Strategies = {'lytic','temperate','lysogenic'};
-% Set default interpreters to 'latex' for consistent font styling
-% This script changes all interpreters from tex to latex. 
+
+%% Set default interpreters to 'latex' for consistent font styling
+%% This script changes all interpreters from tex to latex. 
 
 list_factory = fieldnames(get(groot,'factory'));
 index_interpreter = find(contains(list_factory,'Interpreter'));
@@ -33,10 +38,12 @@ for i = 1:length(index_fontname)
     set(groot, default_name,'Times New Roman');
 end
 
-% Optionally, set a consistent font style, e.g., 'Times New Roman'
+%% Optionally, set a consistent font style, e.g., 'Times New Roman'
 set(groot, 'defaultAxesFontName', 'Times New Roman');
 set(groot, 'defaultTextFontName', 'Times New Roman');
 set(groot,'defaultLineMarkerSize',8);
+
+%% Colormap for PIP plots
 PIPColorMap = [.4 .4 .4;... %invasion invalid because no resident
                1 1 1;... %invasion failed
                .8 .8 .8;...%resident =  mutant
--- Code/lib/fixedparameters.m
+++ Code/lib/fixedparameters.m
@@ -1,25 +1,25 @@
 %% This script generates the parameters (life history traits and simulation parameters) that are kept constant across simulations.
 
 %% Life history parameters (units of hours, micrograms and mL). 
-params.J = 0; %ug/mL-h
-params.conversion_efficiency = 5e-7; %ug/cell
-params.d_R = 0; % per hour
-params.mu_max = 1.2; % per hour
-params.R_in = 4; %ug/mL
-params.alpha_l = 0;
-params.alpha_e = 0;
-params.alpha_i = 0;
+params.J = 0; %ug/mL-h %resource inflow rate
+params.conversion_efficiency = 5e-7; %ug/cell %% amount of resources that need to be consumed to create 1 cell
+params.d_R = 0; % per hour %decay rate of resources
+params.mu_max = 1.2; % per hour %maximal cell growth rate
+params.R_in = 4; %ug/mL %resource density at half max cell growth rate
+params.alpha_l = 0; % fitness advantage of lysogens over susceptible cells
+params.alpha_e = 0; % fitness advantage of exposed cells over susceptible cells
+params.alpha_i = 0; % fitness advantage of lytically infected cells over susceptible cells
 
-params.d_S = .2; %per hour
-params.d_E = .2; %per hour
-params.d_L = .2; %per hour
-params.d_I = .2; %per hour
-params.m = 1/24; %per hour
+params.d_S = .2; %per hour %death rate of susceptible cells
+params.d_E = .2; %per hour %death rate of exposed cells
+params.d_L = .2; %per hour %death rate of lysogens
+params.d_I = .2; %per hour %death rate of lytically infected cells
+params.m = 1/24; %per hour %virion decay rate
 
-params.phi = 3.4e-10; %mL/hr
-params.lambda = 2; %per hour
-params.eta = 1; %per hour
-params.bet = 50;
+params.phi = 3.4e-10; %mL/hr %adsorption rate
+params.lambda = 2; %per hour %transition rate (from exposed to lytic or lysogenic cell)
+params.eta = 1; %per hour %lysis rate
+params.bet = 50; % burst size
 
 
 %% Simulation parameters:
@@ -28,7 +28,7 @@ params.dt = 1/30; % hours
 MaxCycles = 50000; % Max number of cycles to steady state before while loop terminates
 InvasionCycles = 10;% Number of cycles in each set to evaluate transients during invasion
 criticaldensitythreshold = 1e-3; % concentration difference below which two concentrations are treated as identical in per mL
-params.flask_volume = 1/criticaldensitythreshold; %volume in mL
+params.flask_volume = 1/criticaldensitythreshold; %volume of system in mL
 
 %% Numerical method related parameters
 options = odeset('AbsTol',1e-8,'RelTol',1e-8,'NonNegative',1:10); %Options for the ODE function call
@@ -37,13 +37,13 @@ options = odeset('AbsTol',1e-8,'RelTol',1e-8,'NonNegative',1:10); %Options for t
 R0 = 1e2; %ug/mL
 S0 = 1e7; % cells/mL
 Va_0 = 1e4; %virions/mL
-Vb_0 = 10*criticaldensitythreshold;
+Vb_0 = 10*criticaldensitythreshold; %viral genome copies/mL
 
 %% Transfer parameters
-q_R = 1;
-q_S = 0;
-q_E = 0;
-q_I = 0;
+q_R = 1; % fraction of resources transferred from one cycle to next
+q_S = 0; % fraction of susceptible cells transferred from one cycle to next
+q_E = 0; % fraction of exposed cells transferred from one cycle to next
+q_I = 0; % fraction of lytically infected cells transferred from one cycle to next
 
 
 %save('fixedparameters.mat');
